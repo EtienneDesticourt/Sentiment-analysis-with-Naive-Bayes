456605f4026d3e55060e90140734ef55c2913a3f
EtienneDesticourt <etienne.desticourt@github.com>
2014-10-12 18:16:55 +0200

    Added some feature selection, exporting method

--- ReviewParser.py
+++ ReviewParser.py
@@ -3,11 +3,23 @@ Created on 1 Oct 2014
 Handles parsing reviews and sorting words in the neg and pos classes.
 @author: Etienne
 '''
-import re
+import re,csv
+
+
+#List compiled from parsing original reviews and looking at the most frequent words
+WORTHLESSWORDS=[',','(',')','\"','might', 'anyone', 'once', 'goes', 'however,', 'our', 'am', 'nothing', 'away', '/>this', 'comedy', 'rather', 'having', 'fact', 'although', 'far', 'yet', 'takes', 'enough', 'music', 'whole', 'wonderful', "isn't", "that's", 'job', 'worth', 'comes', 'true', 'played', 'found', 'interesting', 'shows', 'plays', ')', 'beautiful', 'fun', 'probably', 'different', 'family', 'done', 'pretty', '"', "can't", 'funny', 'since', 'last', 'feel', 'each', 'especially', 'it,', 'long', 'gets', "i've", 'times', 'thought', 'seems', 'director', 'right', 'thing', 'down', 'excellent', 'almost', 'want', 'role', 'around', 'big', 'must', 'bad', "i'm", 'come', 'give', 'got', 'again', 'bit', 'why', 'without', 'take', 'us', 'going', 'things', 'saw', '/>i', 'part', 'movie,', 'may', 'look', 'now', 'between', 'own', 'another', 'though', 'watching', 'same', 'young', 'always', 'here', 'something', 'show', 'before', 'scene', 'scenes', 'lot', 'both', 'say', 'should', 'off', 'film,', 'find', 'man', 'your', 'through', 'end', 'go', 'back', '-0', 'those', 'makes', 'years', 'these', 'over', 'while', 'did', 'ever', 'know', 'movies', 'then', 'films', 'where', 'does', 'any', 'still', 'being', 'could', 'them', 'two', 'too', 'make', 'him', 'seen', 'she', '/>the', 'way', 'after', 'made', 'we', 'because', 'people', 'do', 'many', 'its', 'how', 'been', 'no', 'were', 'me', 'get', 'other', 'her', 'had', 'will', 'also', 'would', 'their', 'which', 'only', 'up', 'time', 'can', 'my', 'see', 'there', "it's", 'what', 'or', 'some', 'if', 'when', 'just', 'they', 'out', 'about', 'so', 'he', 'has', 'who', 'his', 'from', 'by', 'all', 'you', 'at', 'an', 'be', 'have', 'movie', 'film', 'one', 'are', '<br', '/><br', 'on', 'was', 'as', 'but', 'with', 'for', 'i', 'that', 'it', 'this', 'in', 'is', 'to', 'of', 'a', 'and', 'the']
+
+def cleanReview(review):
+    #Preparsing word removal
+    for word in WORTHLESSWORDS:
+        while word in review:
+            review = review.replace(word,"")
+    return review    
 
     
 def parseReview(review,dim=1):
     review = review.lower() #For now we ignore the difference between Great, GREAT and great.
+    review = cleanReview(review)    
     #Split by sentence to avoid linking unrelated words if I decide to get higher dimensional features later on.
     punctuation = re.compile("[?.!]") #Should probably add parentheses handling later
     sentences = punctuation.split(review)
@@ -30,6 +42,15 @@ def parseReview(review,dim=1):
     return words
 
 
+def cleanDataset(dataset): #Only decreases accuracy
+    newDataset = [{},{}]
+    for i in xrange(len(dataset)):
+        for feature in dataset[i]:
+            if dataset[i][feature] > 100:
+                newDataset[i][feature] = dataset[i][feature]
+    return newDataset
+
+
 def parseDataset(dataset,dim=1):        
     classes = [{},{}] #positive,negative        
     
@@ -43,3 +64,20 @@ def parseDataset(dataset,dim=1):
                 classes[sentiment][word] = 1
                 
     return classes
+
+def exportClasses(classes): #To get some insight into word frquencies and help with feature selection.
+    f = open('features2.csv','wb') #wb to avoid emtpy row every other row
+    writer = csv.writer(f)
+    #Format in row tuples
+    col1=list(classes[0])
+    col2=list(classes[0].values())
+    rows = zip(col1,col2)
+    rows.sort(key=lambda tup: tup[1],reverse=True) #Sort by value
+    #Write rows, no write col options
+    writer.writerows(rows)
+    f.close()
+    
+    
+    
+    
+    
--- main.py
+++ main.py
@@ -4,27 +4,33 @@ Created on 1 Oct 2014
 '''
 
 from NBClassifier import NBClassifier
-from ReviewParser import parseDataset, parseReview
+from ReviewParser import parseDataset, parseReview, exportClasses, cleanDataset
 from DatasetLoader import loadDataset, loadReview
 
 import os, random
 
-TRAINPATHS = ["E:\\Program Files\\Dev\\workspace\\Sentiment analysis with Naive Bayes\\aclImdb\\train\\pos",
-         "E:\\Program Files\\Dev\\workspace\\Sentiment analysis with Naive Bayes\\aclImdb\\train\\neg"]
+TRAINPATHS = ["C:\\Users\\Etienne\\workspace\\Sentiment-analysis-with-Naive-Bayes\\aclImdb\\train\\pos",
+         "C:\\Users\\Etienne\\workspace\\Sentiment-analysis-with-Naive-Bayes\\aclImdb\\train\\neg"]
 
-TESTPATHS = ["E:\\Program Files\\Dev\\workspace\\Sentiment analysis with Naive Bayes\\aclImdb\\test\\pos",
-         "E:\\Program Files\\Dev\\workspace\\Sentiment analysis with Naive Bayes\\aclImdb\\test\\neg"]
+TESTPATHS = ["C:\\Users\\Etienne\\workspace\\Sentiment-analysis-with-Naive-Bayes\\aclImdb\\test\\pos",
+         "C:\\Users\\Etienne\\workspace\\Sentiment-analysis-with-Naive-Bayes\\aclImdb\\test\\neg"]
 
 
 
 
 #Train
-import time
-start = time.time()
 dataset = loadDataset(TRAINPATHS)
+# print "Done loading"
+
 classes = parseDataset(dataset)
-print time.time()-start
-print len(classes[0])+len(classes[1])
+# print "Len before cleaning:", len(classes[0])
+# # classes = cleanDataset(classes)
+# print "Len after cleaning:", len(classes[0])
+# print "Done parsing"
+
+# exportClasses(classes)
+# print "Done exporting"
+
 NBC = NBClassifier(classes,len(dataset))
 
 #Test
